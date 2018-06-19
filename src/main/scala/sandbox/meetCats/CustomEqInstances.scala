fc9c9dfb6655e3e0bcd64f0a314604e40750743c
makoegel <marion-koegel@gmx.de>
2018-06-19 14:29:00 +0200

    fix warnings

--- src/main/scala/sandbox/meetCats/CustomEqInstances.scala
+++ src/main/scala/sandbox/meetCats/CustomEqInstances.scala
@@ -10,8 +10,8 @@ object CustomEqInstances {
 
   implicit val dateEq: Eq[LocalDateTime] =
     Eq.instance[LocalDateTime] { (date1, date2) =>
-      val date1seconds: Long = date1.atZone(ZoneId.systemDefault).toInstant().toEpochMilli
-      val date2seconds: Long = date2.atZone(ZoneId.systemDefault).toInstant().toEpochMilli
+      val date1seconds: Long = date1.atZone(ZoneId.systemDefault).toInstant.toEpochMilli
+      val date2seconds: Long = date2.atZone(ZoneId.systemDefault).toInstant.toEpochMilli
       println("date1Long: " + date1seconds + " date2Long " + date2seconds)
       date1seconds === date2seconds
     }
