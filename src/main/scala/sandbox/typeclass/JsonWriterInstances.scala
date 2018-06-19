6661db179fcb510b05d24c41ecea0d26c55a20b0
makoegel <marion-koegel@gmx.de>
2018-06-19 10:17:56 +0200

    refactor classes

---
+++ src/main/scala/sandbox/typeclass/JsonWriterInstances.scala
@@ -0,0 +1,21 @@
+package sandbox.typeclass
+
+import sandbox.model.Person
+
+// Type class Instances
+object JsonWriterInstances {
+
+    implicit val stringWriter: JsonWriter[String] =
+      new JsonWriter[String] {
+        override def write(value: String): Json = JsString(value)
+      }
+
+    implicit val personWriter: JsonWriter[Person] =
+      new JsonWriter[Person] {
+        override def write(value: Person): Json =
+          JsObject(Map (
+            "name" -> JsString(value.name),
+            "email" -> JsString(value.email)
+          ))
+      }
+}
