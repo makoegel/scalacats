02ad7bd296af5ae8334231793edcb16d15ab6224
makoegel <marion-koegel@gmx.de>
2018-06-20 10:37:22 +0200

    add Monoid

---
+++ src/main/scala/sandbox/typeclasses/Monoid.scala
@@ -0,0 +1,15 @@
+package sandbox.typeclasses
+
+trait Monoid[A] {
+  def combine(x: A, y: A): A
+  def empty: A
+}
+
+object Monoid {
+
+  def associativeLaw[A](x: A, y: A, z: A)(implicit m: Monoid[A]): Boolean =
+    m.combine(x, m.combine(y, z)) == m.combine(m.combine(x, y), z)
+
+  def identityLaw[A](x: A)(implicit m: Monoid[A]): Boolean =
+    (m.combine(x, m.empty) == x) && (m.combine(m.empty, x) == x)
+}
