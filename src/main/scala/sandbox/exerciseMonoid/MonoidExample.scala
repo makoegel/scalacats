ca52d9bfa37b8f9561a8c7f9812d6fe4f3a1788e
makoegel <marion-koegel@gmx.de>
2018-06-20 11:59:58 +0200

    remove comment

--- src/main/scala/sandbox/exerciseMonoid/MonoidExample.scala
+++ src/main/scala/sandbox/exerciseMonoid/MonoidExample.scala
@@ -1,6 +1,6 @@
 package sandbox.exerciseMonoid
 
-import sandbox.typeclasses.{Monoid}
+import sandbox.typeclasses.Monoid
 import sandbox.typeclasses.instances.MonoidInstances._
 
 object MonoidExample {
@@ -13,8 +13,6 @@ object MonoidExample {
     println("Combine Set Monoid with union: " + intSetMonoid.combine(Set(1, 2), Set(2, 3)))
     println("Combine Set Monoid with union: " + intSetMonoid.combine(Set(1, 2), Set.empty))
     println("Empty Set Monoid: " + intSetMonoid.empty)
-    //val intSetSemigroup: Semigroup[Set[Int]] = Semigroup[Set[Int]]
-    //println("Combine Set Semigroup with intersect: " + intSetSemigroup.combine(Set(1, 2, 3), Set(2, 3, 4, 5)))
 
     //val booleanMonoid = Monoid[Boolean]
     // ambiguos implicit values -> booleanAndMonoid - booleanOrMonoid
