21fbf2654dde091e6be654ea827f2f64867b516e
makoegel <marion-koegel@gmx.de>
2018-06-22 22:05:17 +0200

    Evidence that future is not referentially transparent

--- src/main/scala/sandbox/exerciseFunctor/ExFuture.scala
+++ src/main/scala/sandbox/exerciseFunctor/ExFuture.scala
@@ -3,17 +3,48 @@ package sandbox.exerciseFunctor
 import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
+import scala.util.Random
 
-object ExFuture extends App {
+//object ExFuture extends App {
+object ExFuture {
 
-  val future: Future[String] =
-    Future(123)
-      .map(n => n + 1)
-      .map(n => n * 2)
-      .map(n => n + "!")
+  def mapFuture(): Unit = {
+    val future: Future[String] =
+      Future(123)
+        .map(n => n + 1)
+        .map(n => n * 2)
+        .map(n => n + "!")
 
-  val result = Await.result(future, 1.second)
+    val result = Await.result(future, 1.second)
 
-  println("Result Future :" + result)
+    println("Result Future :" + result)
+  }
+
+  def randomFuture(): Unit = {
+    val future1 = {
+      val r = new Random(0L)
+      val x = Future(r.nextInt)
+
+      for {
+        a <- x
+        b <- x
+      } yield (a, b)
+    }
+
+    val future2 = {
+      val r = new Random(0L)
+
+      for {
+        a <- Future(r.nextInt)
+        b <- Future(r.nextInt) // new Call for Future : side-effect, not referentially transparent
+      } yield (a, b)
+    }
+
+    println("result1: " + Await.result(future1, 1.second))
+    println("result2: " + Await.result(future2, 1.second))
+  }
+
+  mapFuture()
+  randomFuture()
 
 }
